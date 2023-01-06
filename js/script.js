7c76ff289e6f9d1c655e91c47be82e92452d2e90
Krishnaanantham Samahithan <krishnaanantham.20210863@iit.ac.lk>
2023-01-06 10:53:39 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    Strings:["Web Designer", "Web Developer", "Software Developer", "Graphic Designer", "Photographer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
